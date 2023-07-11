b989dd4d0f2a8d927f2cc6940811004e37f78624
Vinoth Dayalan <113970123+VinothDayalan@users.noreply.github.com>
2023-07-11 22:36:11 +0530

    CIntro v1.0

---
+++ CIntro-1/HelloWorld-1.c
@@ -0,0 +1,8 @@
+// # - is used to indicate preprocessor directory such as include, define and etc.
+// include - this just adds the header to the code ( copies & paste the header library).
+// Stdio.h - System defined library which is inbuilt - operations done scanf(), printf(), fopen() and etc. 
+#include <stdio.h>
+int main(){
+    printf("Hello World!");
+    return 0;
+}
---
+++ CIntro-1/HelloWorld-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+// int main() - function mandatory,
+//              Entry point of a C program,
+//              Serves as the starting point for a code in c.
+int main(){
+    printf("Hello World!");
+    // return 0 - Indicates that the program execution is completed successfully.
+    // 0 - Status of the program.
+    return 0;
+}
